3ea3c4092a692ea858edd20963d5ed9a1a9bc32a
Lakain <60016343+Lakain@users.noreply.github.com>
2024-03-25 18:24:11 -0500

    bug fix
    bug fixed

--- OT_HZ_Invoice2import.py
+++ OT_HZ_Invoice2import.py
@@ -14,10 +14,10 @@ df_receiving.to_csv(f'{invoiceNo}_receiving.csv', index=False)
 
 df_new_item = df[['Barcode']]
 df_new_item.insert(loc=len(df_new_item.columns),column= 'Description', value=df['Item'].str.cat(' '+df['Color']))
-df_new_item.insert(loc=len(df_new_item.columns), column= 'Ext Desc', value=df['Item'].str.cat(' #'+df['Color']))
+df_new_item.insert(loc=len(df_new_item.columns), column= 'Ext Desc', value=df['Name'].str.cat(' #'+df['Color']))
 df_new_item.insert(loc=len(df_new_item.columns), column='Cost', value=df['Price'].str.strip('$ '))
 df_new_item.insert(loc=len(df_new_item.columns), column='Regular Price', value=0)
 df_new_item.insert(loc=len(df_new_item.columns), column='Reorder No', value=df['Item'])
 df_new_item.insert(loc=len(df_new_item.columns), column='Department', value=pd.NA)
 
-df_receiving.to_csv(f'{invoiceNo}_new_item.csv', index=False)
+df_new_item.to_csv(f'{invoiceNo}_new_item.csv', index=False)
