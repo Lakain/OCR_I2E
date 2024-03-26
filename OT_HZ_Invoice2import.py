3ed45c2575c0c789448e495aa33c8c463509d879
Lakain <60016343+Lakain@users.noreply.github.com>
2024-03-26 12:55:46 -0500

    Update OT_HZ_Invoice2import.py
    Supplier update file generate function added.

--- OT_HZ_Invoice2import.py
+++ OT_HZ_Invoice2import.py
@@ -13,11 +13,18 @@ df_receiving.loc[:, 'Cost'] = df_receiving['Cost'].str.strip('$ ')
 df_receiving.to_csv(f'{invoiceNo}_receiving.csv', index=False)
 
 df_new_item = df[['Barcode']]
-df_new_item.insert(loc=len(df_new_item.columns),column= 'Description', value=df['Item'].str.cat(' '+df['Color']))
-df_new_item.insert(loc=len(df_new_item.columns), column= 'Ext Desc', value=df['Name'].str.cat(' #'+df['Color']))
+df_new_item.insert(loc=len(df_new_item.columns),column='Description', value=df['Item'].str.cat(' '+df['Color']))
+df_new_item.insert(loc=len(df_new_item.columns), column='Ext Desc', value=df['Name'].str.cat(' #'+df['Color']))
 df_new_item.insert(loc=len(df_new_item.columns), column='Cost', value=df['Price'].str.strip('$ '))
 df_new_item.insert(loc=len(df_new_item.columns), column='Regular Price', value=0)
-df_new_item.insert(loc=len(df_new_item.columns), column='Reorder No', value=df['Item'])
 df_new_item.insert(loc=len(df_new_item.columns), column='Department', value=pd.NA)
 
 df_new_item.to_csv(f'{invoiceNo}_new_item.csv', index=False)
+
+df_supplier = df[['Barcode']]
+df_supplier.insert(loc=len(df_supplier.columns),column='Cost', value=df['Price'])
+df_supplier.insert(loc=len(df_supplier.columns),column='ReorderNo', value=df['Item'])
+df_supplier.insert(loc=len(df_supplier.columns),column='MinimumOrder', value=0)
+df_supplier.insert(loc=len(df_supplier.columns),column='MPQ', value=0)
+
+df_supplier.to_csv(f'{invoiceNo}_supplier.csv', index=False)
