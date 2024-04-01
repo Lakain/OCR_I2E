11dfee868afb583ec13c856e3733f5d4d182985a
Lakain <60016343+Lakain@users.noreply.github.com>
2024-04-01 15:55:15 -0500

    Feature Implemented
    # Feature implemented BY_invocie2excel
    
    # output format changed to TSV for compatibility
    
    # FIle renamed

--- OT_HZ_Invoice2excel.py
+++ OT_HZ_Invoice2excel.py
@@ -1,16 +1,19 @@
 from tkinter.filedialog import askopenfilename
 import pandas as pd
+import os
 
 path = askopenfilename(initialdir='.')
 
 df = pd.read_html(path, displayed_only=False)[0]
 
 invoiceNo = df['InvoiceCode'][0]
+os.mkdir('./'+invoiceNo)
+
 df_receiving = df[['Barcode', 'Qty', 'Price']]
 df_receiving.columns=['Code', 'Qty on Ord', 'Cost']
 df_receiving.loc[:, 'Cost'] = df_receiving['Cost'].str.strip('$ ')
 
-df_receiving.to_csv(f'{invoiceNo}_receiving.csv', index=False)
+df_receiving.to_csv(f'{invoiceNo}/{invoiceNo}_receiving.txt', sep='\t',index=False)
 
 df_new_item = df[['Barcode']]
 df_new_item.insert(loc=len(df_new_item.columns),column='Description', value=df['Item'].str.cat(' '+df['Color']))
@@ -19,7 +22,7 @@ df_new_item.insert(loc=len(df_new_item.columns), column='Cost', value=df['Price'
 df_new_item.insert(loc=len(df_new_item.columns), column='Regular Price', value=0)
 df_new_item.insert(loc=len(df_new_item.columns), column='Department', value=pd.NA)
 
-df_new_item.to_csv(f'{invoiceNo}_new_item.csv', index=False)
+df_new_item.to_csv(f'{invoiceNo}/{invoiceNo}_new_item.txt', sep='\t', index=False)
 
 df_supplier = df[['Barcode']]
 df_supplier.insert(loc=len(df_supplier.columns),column='Cost', value=df['Price'])
@@ -27,4 +30,4 @@ df_supplier.insert(loc=len(df_supplier.columns),column='ReorderNo', value=df['It
 df_supplier.insert(loc=len(df_supplier.columns),column='MinimumOrder', value=0)
 df_supplier.insert(loc=len(df_supplier.columns),column='MPQ', value=0)
 
-df_supplier.to_csv(f'{invoiceNo}_supplier.csv', index=False)
+df_supplier.to_csv(f'{invoiceNo}/{invoiceNo}_supplier.txt', sep='\t', index=False)
